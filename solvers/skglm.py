8525d20d6285e5c13ce6cc9c67ae6b8f18a430fe
Badr-MOUFAD <badr.moufad@emines.um6p.ma>
2023-05-24 16:05:16 +0200

    add skglm solver

---
+++ solvers/skglm.py
@@ -0,0 +1,43 @@
+from benchopt import BaseSolver, safe_import_context
+
+with safe_import_context() as import_ctx:
+    from skglm.datafits import Cox
+    from skglm.penalties import L1
+    from skglm.solvers import ProxNewton
+    from skglm.utils.jit_compilation import compiled_clone
+
+
+class Solver(BaseSolver):
+
+    name = 'skglm'
+
+    requirements = [
+        "git+https://github.com/Badr-MOUFAD/skglm.git@cox-estimator",
+    ]
+
+    stopping_criterion = 'iteration'
+
+    def set_objective(self, tm, s, X, alpha):
+        self.tm, self.s, self.X = tm, s, X
+
+        # fit ProxNewton
+        self.datafit = compiled_clone(Cox())
+        self.penalty = compiled_clone(L1(alpha))
+
+        self.datafit.initialize(X, (tm, s))
+        self.solver = ProxNewton(fit_intercept=False, tol=1e-9)
+
+        # cache numba compilation
+        self.run(4)
+
+    def run(self, n_iter):
+        self.solver.max_iter = n_iter
+
+        w, *_ = self.solver.solve(
+            self.X, (self.tm, self.s), self.datafit, self.penalty
+        )
+
+        self.w = w
+
+    def get_result(self):
+        return self.w
