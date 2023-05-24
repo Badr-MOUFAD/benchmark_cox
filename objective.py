12a798c2c3b762655f5ed69deb56faa50cd9f5b9
Badr-MOUFAD <badr.moufad@emines.um6p.ma>
2023-05-24 16:23:20 +0200

    implement obj

--- objective.py
+++ objective.py
@@ -1,66 +1,55 @@
 from benchopt import BaseObjective, safe_import_context
 
-# Protect the import with `safe_import_context()`. This allows:
-# - skipping import to speed up autocompletion in CLI.
-# - getting requirements info when all dependencies are not installed.
 with safe_import_context() as import_ctx:
     import numpy as np
+    from numpy.linalg import norm
 
 
-# The benchmark objective must be named `Objective` and
-# inherit from `BaseObjective` for `benchopt` to work properly.
 class Objective(BaseObjective):
 
-    # Name to select the objective in the CLI and to display the results.
-    name = "Ordinary Least Squares"
+    name = "L1 Cox Estimation"
 
-    # List of parameters for the objective. The benchmark will consider
-    # the cross product for each key in the dictionary.
-    # All parameters 'p' defined here are available as 'self.p'.
-    # This means the OLS objective will have a parameter `self.whiten_y`.
     parameters = {
-        'whiten_y': [False, True],
+        'reg': [1e-1, 1e-2, 1e-3],
     }
 
-    # Minimal version of benchopt required to run this benchmark.
-    # Bump it up if the benchmark depends on a new feature of benchopt.
     min_benchopt_version = "1.3"
 
-    def set_data(self, X, y):
-        # The keyword arguments of this function are the keys of the dictionary
-        # returned by `Dataset.get_data`. This defines the benchmark's
-        # API to pass data. This is customizable for each benchmark.
-        self.X, self.y = X, y
+    def __init__(self, reg):
+        self.reg = reg
 
-        # `set_data` can be used to preprocess the data. For instance,
-        # if `whiten_y` is True, remove the mean of `y`.
-        if self.whiten_y:
-            y -= y.mean(axis=0)
+    def set_data(self, tm, s, X):
+        self.tm, self.s, self.X = tm, s, X
 
-    def compute(self, beta):
-        # The arguments of this function are the outputs of the
-        # `Solver.get_result`. This defines the benchmark's API to pass
-        # solvers' result. This is customizable for each benchmark.
-        diff = self.y - self.X.dot(beta)
+        self.B = (tm >= tm[:, None]).astype(float)
+        self.alpha = self.reg * Objective._compute_alpha_max(tm, s, X)
 
-        # This method can return many metrics in a dictionary. One of these
-        # metrics needs to be `value` for convergence detection purposes.
+    def compute(self, w):
+        s, X = self.s, self.X
+
+        Xw = X @ w
+        minus_log_lik = -(s @ Xw) + s @ np.log(self.B @ np.exp(Xw))
+        penalty_val = self.alpha * norm(w, ord=1)
+
         return dict(
-            value=.5 * diff.dot(diff),
+            value=minus_log_lik + penalty_val,
+            support_size=(w != 0).sum(),
         )
 
     def get_one_solution(self):
-        # Return one solution. The return value should be an object compatible
-        # with `self.compute`. This is mainly for testing purposes.
         return np.zeros(self.X.shape[1])
 
+    @staticmethod
+    def _compute_alpha_max(tm, s, X):
+        n_samples = X.shape[0]
+
+        B = (tm >= tm[:, None]).astype(X.dtype)
+        grad_0 = -s + B.T @ (s / np.sum(B, axis=1))
+
+        return norm(X.T @ grad_0, ord=np.inf) / n_samples
+
     def get_objective(self):
-        # Define the information to pass to each solver to run the benchmark.
-        # The output of this function are the keyword arguments
-        # for `Solver.set_objective`. This defines the
-        # benchmark's API for passing the objective to the solver.
-        # It is customizable for each benchmark.
+
         return dict(
-            X=self.X,
-            y=self.y,
+            tm=self.tm, s=self.s, X=self.X, alpha=self.alpha,
         )
