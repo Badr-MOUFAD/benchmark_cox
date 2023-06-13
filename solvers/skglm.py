ae6388a64f6de11435f7079f73fe6c393ed90c26
Badr-MOUFAD <badr.moufad@emines.um6p.ma>
2023-06-13 12:44:19 +0200

    add skglm:prox newton

--- solvers/skglm.py
+++ solvers/skglm.py
@@ -3,8 +3,8 @@ from benchopt import BaseSolver, safe_import_context
 
 with safe_import_context() as import_ctx:
     from skglm.datafits import Cox
-    from skglm.penalties import L1_plus_L2
-    from skglm.solvers import ProxNewton
+    from skglm.penalties import L1_plus_L2, L2
+    from skglm.solvers import ProxNewton, LBFGS
     from skglm.utils.jit_compilation import compiled_clone
 
 
@@ -12,25 +12,36 @@ class Solver(BaseSolver):
 
     name = 'skglm'
 
-    # TODO: replace `pip:git+https://github.com/Badr-MOUFAD/skglm.git`
-    # after merging skglm PR 159
+    parameters = {
+        "solver": ["Prox-Newton", 'L-BFGS']
+    }
+
     requirements = [
-        "pip:git+https://github.com/Badr-MOUFAD/skglm.git@cox-efron",
+        "pip:git+https://github.com/scikit-learn-contrib/skglm.git@main",
     ]
 
     stopping_strategy = 'iteration'
 
     def set_objective(self, tm, s, X, alpha, l1_ratio, use_efron):
         self.tm, self.s, self.X = tm, s, X
-
-        # fit ProxNewton
-        self.datafit = compiled_clone(Cox(use_efron))
-        self.penalty = compiled_clone(L1_plus_L2(alpha, l1_ratio))
-
-        self.datafit.initialize(X, (tm, s))
+        self.l1_ratio = l1_ratio
 
         warnings.filterwarnings('ignore')
-        self.solver = ProxNewton(fit_intercept=False, tol=1e-9)
+        if self.solver == "Proximal-Newton":
+            # fit ProxNewton
+            self.datafit = compiled_clone(Cox(use_efron))
+            self.penalty = compiled_clone(L1_plus_L2(alpha, l1_ratio))
+
+            self.datafit.initialize(X, (tm, s))
+            self.solver = ProxNewton(fit_intercept=False, tol=1e-9)
+        elif self.solver == "L-BFGS":
+            # L-BFGS
+            self.datafit = compiled_clone(Cox(use_efron))
+            self.penalty = compiled_clone(L2(alpha))
+
+            self.datafit.initialize(X, (tm, s))
+
+            self.solver = LBFGS(tol=1e-9)
 
         # cache numba compilation
         self.run(4)
@@ -41,14 +52,20 @@ class Solver(BaseSolver):
         w, *_ = self.solver.solve(
             self.X, (self.tm, self.s), self.datafit, self.penalty
         )
-
         self.w = w
 
     def get_result(self):
         return self.w
 
     def skip(self, tm, s, X, alpha, l1_ratio, use_efron):
-        if alpha == 0.:
-            return True, f"{self.name} does not handle unpenalized Cox estimation."
+        if alpha == 0. and self.solver == "Prox-Newton":
+            reason = (f"{self.name}:{self.solver} does not handle"
+                      " unpenalized Cox estimation.")
+            return True, reason
+
+        if l1_ratio != 0. and self.solver == "L-BFGS":
+            reason = (f"{self.name}:{self.solver} handles only "
+                      "L2 Cox regularization")
+            return True, reason
 
         return False, None
